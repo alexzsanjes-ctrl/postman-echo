bd3521c1074f33dc219f6d410ad051fffd6803da
Александр Саломанин <alexzsanjes@gmail.com>
2026-03-04 21:20:17 +0700

    test modifided

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
